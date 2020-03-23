52953a2478e2d8aeb249d0c0a8c5c39266815c69
surekha0495 <surekha.0495@gmail.com>
2020-03-22 23:15:15 -0700

    added to the repositary

---
+++ src/org/cts/Ayaan.java
@@ -0,0 +1,19 @@
+package org.cts;
+
+public class Ayaan {
+private void hasi() {
+	System.out.println("java");
+
+}private void karthi() {
+	System.out.println("oracle");
+
+}private void tanay() {
+	System.out.println("selenium");
+
+}public static void main(String[] args) {
+	Ayaan a=new Ayaan();
+	a.hasi();
+	a.karthi();
+	a.tanay();
+}
+}
